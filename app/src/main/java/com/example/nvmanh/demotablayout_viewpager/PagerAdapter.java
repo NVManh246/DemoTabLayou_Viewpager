93e4d482b72b57c5f930d60bb5b32261a8b1de80
NVManh <nvmanhopb@gmail.com>
2018-07-13 09:57:51 +0700

    demo3

---
+++ app/src/main/java/com/example/nvmanh/demotablayout_viewpager/PagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.nvmanh.demotablayout_viewpager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> mFragments;
+    private List<String> mTitles;
+
+    public PagerAdapter(FragmentManager fm, List<Fragment> mFragments, List<String> mTitles) {
+        super(fm);
+        this.mFragments = mFragments;
+        this.mTitles = mTitles;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mTitles.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitles.get(position);
+    }
+}
